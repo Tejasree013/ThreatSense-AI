53a75401abde8b0d200ffc92f55b96926b8e29a1
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-06-22 12:35:03 +0530

    Added environment variable support

--- fake_news/live_fact_check.py
+++ fake_news/live_fact_check.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import os
 import requests
+
+load_dotenv()
 
 API_KEY = os.getenv("NEWS_API_KEY")
 
--- test_gemini.py
+++ test_gemini.py
@@ -1,7 +1,12 @@
 import google.generativeai as genai
-
+from dotenv import load_dotenv
 import os
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
